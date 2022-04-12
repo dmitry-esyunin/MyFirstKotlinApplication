9811d00921f3374a94401498e1f3fa6675a4aee0
Dmitry Esyunin <74874618+dmitry-esyunin@users.noreply.github.com>
2022-04-12 14:43:48 +0300

    init

---
+++ app/src/main/java/com/gbkotlin/myfirstkotlinapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gbkotlin.myfirstkotlinapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gbkotlin.myfirstkotlinapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
